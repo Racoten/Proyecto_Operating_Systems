5c6735968eb1a22a395f1ad22ed5a93cbe1b67eb
Faustino R Fuertes <104951410+ffuertes1@users.noreply.github.com>
2023-04-12 19:07:09 +0000

    SSTF DONE

--- disk.h
+++ disk.h
@@ -17,25 +17,37 @@ int dFCFS(int requests[]) {
     return total_moves;
 }
 int sstf(int requests[]) {
+    // Set the initial head position as the first request
     int head = requests[0];
+    // Initialize the total number of disk movements to 0
     int total_moves = 0;
+    // Initialize the current position as the head position
     int curr_pos = head;
 
+    // Print a message to indicate that the sequence of disk movements is being printed
     cout << "Sequence of movements: ";
+
+    // Process all disk requests
     for (int i = 0; i < 9; i++) {
-        int min_dist = INT_MAX;
-        int closest_request;
-        for (int j = i; j < 9; j++) {
-            if (abs(requests[j] - curr_pos) < min_dist) {
-                closest_request = requests[j];
-                min_dist = abs(requests[j] - curr_pos);
-                 cout << curr_pos << " -> ";
-                
-            }
-        }
+        // Sort the remaining requests in ascending order of distance from the current position
+        sort(requests + i, requests + 9, [curr_pos](int a, int b) {
+            return abs(a - curr_pos) < abs(b - curr_pos);
+        });
+
+        // Select the request with the smallest distance from the current position
+        int closest_request = requests[i];
+        // Calculate the distance between the current position and the selected request
+        int min_dist = abs(closest_request - curr_pos);
+
+        // Update the total number of disk movements
         total_moves += min_dist;
+        // Update the current position to the selected request
         curr_pos = closest_request;
-        swap(requests[i], requests[distance(requests, find(requests, requests + 9, closest_request))]);
+
+        // Print the selected request as the next disk movement
+        cout << curr_pos << " -> ";
     }
+
+    // Return the total number of disk movements
     return total_moves;
 }
